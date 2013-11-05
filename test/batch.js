3776caeaf48b877751c61919bc1c19fc4a113699
Victor Quinn <victor@socialradar.com>
2013-11-05 22:22:54 +0000

    Add some fake endpoints to our helper app, start implementing the batch middleware

--- test/batch.js
+++ test/batch.js
@@ -17,5 +17,22 @@ describe('batch', function() {
             expect(batch).to.be.a('function');
         });
     });
+
+    describe('test our app helper', function() {
+        it('has a /users/1/name endpoint', function(done) {
+            request(app)
+                .get('/users/1/name')
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.exist;
+                    done();
+                });
+        });
+    });
+
+    describe('basic', function() {
+        it('can handle a single request');
+        
+    });
 });
 
--- test/helpers/app.js
+++ test/helpers/app.js
@@ -1,9 +1,26 @@
 // Test app
 var express = require('express'),
     app = module.exports = express(),
+    Chance = require('chance'),
+    chance = new Chance(),
     batch = require('../../lib/batch-request')();
 
 app.use(express.json());
 
+// An endpoint to use the batch middleware
 app.post('/batch', batch.validate, batch);
+
+// Let's make some fake endpoints
+app.get('/users/:id/name', function(req, res) {
+    res.json(chance.name());
+});
+
+app.get('/users/:id/email', function(req, res) {
+    res.json(chance.email());
+});
+
+app.get('/users/:id/company', function(req, res) {
+    res.json(chance.capitalze(chance.word()));
+});
+
 app.listen(3000);
